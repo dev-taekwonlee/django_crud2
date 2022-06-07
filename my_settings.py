c2c20d9dbb3657d91a12479b9647b057fc2efc54
dev-taekwonlee <dev.taekwonlee@gmail.com>
2022-06-07 14:39:03 +0900

    CREATED: Project initial settings, it's working normally.

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud2',
+        'USER': 'root',
+        'PASSWORD': 'xornjszja',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-s%d&k*9g^f(io#n0895_h8mmq+qgs*jex%pe)d*l4q5qwz++_k'
